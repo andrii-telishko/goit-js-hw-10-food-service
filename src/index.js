3351fdd5f2e04b29658ff84d588fb6d44d8a7cfb
Andrii Telishko <telishkoav@gmail.com>
2021-03-23 00:14:41 +0200

    додав зміну теми

--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
 import menuListTemplate from './template/template.hbs'
 import menuList from './menu.json'
-console.log(menuList);
-
-console.log(menuList[0].ingredients[0]);
-
-console.log(menuListTemplate);
 
 const menuRef = document.querySelector('.js-menu');
+const darkThemeCheckBoxRef = document.getElementById('theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+
+const markupTemplate = menuListTemplate(menuList);
+menuRef.insertAdjacentHTML('beforeend', markupTemplate);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const { LIGHT, DARK } = Theme;
+
+bodyRef.classList.add(LIGHT);
+
+let darkTheme = localStorage.getItem('darkTheme');
+
+const turnOnDarkTheme = () => {
+  bodyRef.classList.remove(LIGHT);
+  bodyRef.classList.add(DARK);
+  localStorage.setItem('darkTheme', 'dark');
+  darkThemeCheckBoxRef.setAttribute('checked', true)
+}
+
+const turnOffDarkTheme = () => {
+   bodyRef.classList.remove(DARK);
+   bodyRef.classList.add(LIGHT);
+   localStorage.setItem('darkTheme', 'light');
+}
+
+if (darkTheme === 'dark') {
+  turnOnDarkTheme();
+}
+
+darkThemeCheckBoxRef.addEventListener('change', onCheckBoxChange);
+
+function onCheckBoxChange() {
+    darkTheme = localStorage.getItem('darkTheme'); 
+     if (darkTheme !== 'dark') {
+       turnOnDarkTheme();
+     } else {  
+       turnOffDarkTheme(); }
+}
 
 
 
-console.log(menuRef);
 
-const markupList = menuListTemplate(menuList);
-console.log(markupList);
 
-menuRef.insertAdjacentHTML('beforeend', markupList);
 
+
+
+   
